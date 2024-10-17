ee44baeee5b72832c9f10c79726ea0dff0bf590a
Kevin M <ygopotato@gmail.com>
2024-10-17 16:37:28 -0500

    fixed the modal

--- src/modal.js
+++ src/modal.js
@@ -1,6 +1,13 @@
 const modal = document.querySelector("#modal");
 const openModal = document.querySelector(".open-button");
+const wrapper = document.querySelector(".notClose");
 
 openModal.addEventListener("click", () => {
   modal.showModal();
+});
+
+modal.addEventListener("click", (e) => {
+  if (!wrapper.contains(e.target)) {
+    modal.close();
+  }
 });
